332465bad1c79b2c61625d8b00e0d4ce197194e6
reza manazil <rezamanazil@gmail.com>
2022-11-05 23:50:36 +0700

    hapus no 4

--- Tugas 2_Pyhton/no4.py
+++
@@ -1,8 +0,0 @@
-n=int(input("masukan nilai n : "))
-for i in range(n):
-    for j in range(n):
-        if(j<n-1 and j>0 and i<n-1 and i>0):
-            print("#",end="")
-        else:
-            print("*",end="")
-    print()
