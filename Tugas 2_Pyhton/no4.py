99917187f8c40dc721a3fb681698b62596101a4b
reza manazil <rezamanazil@gmail.com>
2022-11-05 23:40:16 +0700

    ubah sedikit

--- Tugas 2_Pyhton/no4.py
+++ Tugas 2_Pyhton/no4.py
@@ -1,1 +1,8 @@
-n=int(input("Masukan nilai n : "))
+n=int(input("masukan nilai n : "))
+for i in range(n):
+    for j in range(n):
+        if(j<n-1 and j>0 and i<n-1 and i>0):
+            print("#",end="")
+        else:
+            print("*",end="")
+    print()
