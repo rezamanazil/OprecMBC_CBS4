bf6790e0ebc0d414e961b1fca9cfcb74eb0618fa
reza manazil <rezamanazil@gmail.com>
2022-11-06 00:05:00 +0700

    Nomer 4

--- Tugas 2_Pyhton/no4.py
+++ Tugas 2_Pyhton/no4.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 x=int(input("masukan nilai n : "))
 for i in range(x):
     for j in range(x):
@@ -7,5 +6,3 @@ for i in range(x):
         else:
             print("*",end="")
     print()
-=======
->>>>>>> 332465bad1c79b2c61625d8b00e0d4ce197194e6
