754e28fbf75bd5c1f14c1f9da9be19574cebd5c0
Galang Aditya <galang11183@gmail.com>
2022-11-05 23:51:20 +0700

    nomor 4

--- Tugas 2_Pyhton/no4.py
+++ Tugas 2_Pyhton/no4.py
@@ -1,7 +1,7 @@
-n=int(input("masukan nilai n : "))
-for i in range(n):
-    for j in range(n):
-        if(j<n-1 and j>0 and i<n-1 and i>0):
+x=int(input("masukan nilai n : "))
+for i in range(x):
+    for j in range(x):
+        if(j<x-1 and j>0 and i<x-1 and i>0):
             print("#",end="")
         else:
             print("*",end="")
